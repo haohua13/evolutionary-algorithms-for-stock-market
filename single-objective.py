29c52d815de83cbb793039c0e11c8e2e0672256c
haohua13 <57109967+haohua13@users.noreply.github.com>
2023-10-22 15:56:53 +0100

    add initial

--- single-objective.py
+++ single-objective.py
@@ -2,20 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import random
+import itertools
 
-
-def main():
-    folder_path = 'ACI_Project2_2324_Data'
-    file_list = os.listdir(folder_path)
-    for file_name in file_list:
-        if file_name.endswith('.csv'):
-            # Extract the base name of the file (without the extension)
-            df_name = os.path.splitext(file_name)[0]
-            # Construct the full file path
-            file_path = os.path.join(folder_path, file_name)
-            
-            # Read the CSV file into a DataFrame with the base name as the variable name
-            globals()[df_name] = pd.read_csv(file_path)
-
-if __name__ == "__main__":
-    main()
